497fd5e1e5b2b8fce5d2b0dfd22df72d1573d781
MitarBrankovic <brankovic.ra198.2018@uns.ac.rs>
2020-11-15 23:35:33 +0100

    StatusBar

---
+++ src/projekat/StatusBar.java
@@ -0,0 +1,42 @@
+package projekat;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Calendar;
+import java.sql.Date;
+import javax.swing.*;
+import javax.swing.Timer;
+import javax.swing.border.BevelBorder;
+import java.text.*;
+
+public class StatusBar extends JPanel {
+
+	public StatusBar() {
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(1400,23));
+		setBorder(new BevelBorder(BevelBorder.LOWERED));
+		
+		JLabel txtLabela = new JLabel("Studentska sluzba ");
+		DateFormat vremeFormat = new SimpleDateFormat(" HH:mm dd.MM.yyyy ");
+		
+		String trenutnoVreme = vremeFormat.format(Calendar.getInstance().getTime());
+		JLabel vremeLabela = new JLabel(trenutnoVreme);
+		
+		ActionListener timerListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date datum = new Date(System.currentTimeMillis());
+				String vreme = vremeFormat.format(datum);
+				vremeLabela.setText(vreme);
+			}	
+		};
+		
+		Timer timer = new Timer(0, timerListener);
+		timer.setInitialDelay(0);
+		timer.start();
+		
+		add(txtLabela, BorderLayout.WEST);
+		add(vremeLabela, BorderLayout.EAST);
+
+	}
+		
+}
