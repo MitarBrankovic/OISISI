783907c67d522a9cd3a8fd6e4b886d86ffb13a2d
SrdjanBeric <srdjan.beric123@gmail.com>
2020-12-14 20:05:20 +0100

    #prikaz_predmeta
    Predmeti.txt i BazaPredmeta

--- src/model/BazaPredmeta.java
+++ src/model/BazaPredmeta.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class BazaPredmeta {
@@ -30,10 +36,50 @@ public class BazaPredmeta {
 	}
 	
 	private void initPredmete() {
-		this.predmeti = new ArrayList<Predmet>();
+		/*this.predmeti = new ArrayList<Predmet>();
 		//	public Predmet(String sifraPredmeta, String nazivPredmeta, int godinaStudija, Profesor predmetniProfesor, int espb) {
 		predmeti.add(new Predmet("E231","Elektronika", 2, "Jasar Ahmedovski", 8, semestar.letnji));
 		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		predmeti.add(new Predmet("E2S1","Analiza 1", 1, "Rale Mrale", 9, semestar.zimski));
+		*/
+		
+		this.predmeti = new ArrayList<Predmet>();
+		String kolone[];
+		String naredni;
+		BufferedReader reader = null;
+		
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream("tabele" + File.separator + "Predmeti.txt")));
+		} catch (FileNotFoundException exception) {
+			exception.printStackTrace();
+		}
+		
+		
+		try {
+			while((naredni = reader.readLine()) != null) {
+				if(naredni.equals(""))	continue;
+				
+				kolone = naredni.split("\\,");
+				
+				semestar semestar1;
+				if(kolone[5].trim().equals("zimski"))
+					semestar1 = semestar.zimski;
+				else
+					semestar1 = semestar.letnji;
+				
+				predmeti.add(new Predmet( kolone[0].trim(), kolone[1].trim(), Integer.parseInt(kolone[2].trim()), kolone[3].trim(), Integer.parseInt(kolone[4].trim()), semestar1));
+				
+			}
+			
+			String s = reader.readLine();
+			System.out.println(s);
+		} catch(IOException exception) {
+			exception.printStackTrace();
+		}
 
 	}
 	
