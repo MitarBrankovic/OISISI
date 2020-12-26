e778450043aaca127232d7f6e4912ba92e6ea577
SrdjanBeric <srdjan.beric123@gmail.com>
2020-12-26 18:52:22 +0100

    #sortiranje_predmeta

--- src/view/PredmetiJTable.java
+++ src/view/PredmetiJTable.java
@@ -24,6 +24,7 @@ public class PredmetiJTable extends JTable{
 		this.setRowSelectionAllowed(true);
 		this.setColumnSelectionAllowed(true);
 		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setAutoCreateRowSorter(true);
 		this.setModel(new AbstractTablePredmeti());
 	}
 
